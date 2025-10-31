628b849b5bcb1462e920f75827f367fb57565023
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-10-31 17:29:37 +0500

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,17 +1,7 @@
 let coins = 0;
-let energy = 5500;
+const coinsEl = document.getElementById("coins");
 
-const coinCount = document.getElementById("coinCount");
-const energyDisplay = document.getElementById("energy");
-const energyFill = document.getElementById("energy-fill");
-const coin = document.getElementById("coin");
-
-coin.addEventListener("click", () => {
-  if (energy > 0) {
-    coins++;
-    energy--;
-    coinCount.textContent = coins.toLocaleString();
-    energyDisplay.textContent = energy;
-    energyFill.style.width = (energy / 5500) * 100 + "%";
-  }
+document.querySelector(".coin").addEventListener("click", () => {
+    coins += 1;
+    coinsEl.textContent = coins.toLocaleString();
 });
