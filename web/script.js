92a37c1e1a47810182b820b46801beb5a8e7cec3
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-11-02 05:25:39 +0500

    Delete web directory

--- web/script.js
+++
@@ -1,7 +0,0 @@
-let coins = 0;
-const coinsEl = document.getElementById("coins");
-
-document.querySelector(".coin").addEventListener("click", () => {
-    coins += 1;
-    coinsEl.textContent = coins.toLocaleString();
-});
